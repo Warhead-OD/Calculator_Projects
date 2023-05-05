b0fbf76b5e255c386b3d55fb9366d5f08e7ffea5
Warhead-OD <SimeonK2007@gmail.com>
2023-05-05 11:57:45 -0600

    Fixes i guess

--- Projects/Oblique Triangle Solver.py
+++ Projects/Oblique Triangle Solver.py
@@ -3,36 +3,41 @@ from math import degrees, pi, sin, asin, cos, acos
 
 try:
 	# This is collecting the information on the triangle from the user
-	angle_a = int(input("m<A: "))
-	angle_b = int(input("m<B: "))
-	angle_c = int(input("m<C: "))
-	side_a = int(input("Side a: "))
-	side_b = int(input("Side b: "))
-	side_c = int(input("Side c: "))
+	a_a = float(input("m<A: "))
+	a_b = float(input("m<B: "))
+	a_c = float(input("m<C: "))
+	s_a = float(input("Side a: "))
+	s_b = float(input("Side b: "))
+	s_c = float(input("Side c: "))
 except ValueError:
 	input("Invalid input. Press 'ENTER'")
+	quit("Invalid FloatI")
 
 
 def main():
-	global angle_a
-	global angle_b
-	global angle_c
-	global side_a
-	global side_b
-	global side_c
+	global a_a
+	global a_b
+	global a_c
+	global s_a
+	global s_b
+	global s_c
 	
-	if side_a * side_b * side_c:
+	if (a_a * a_b * a_c) and (a_a + a_b + a_c != 180):
+		input("Invalid angle input.\nPress 'ENTER' to continue.")
+		quit("Invalid AngleI")
+	
+	if s_a * s_b * s_c:
 		def sss_lofc(a, b, c):
 			return degrees(acos((c ** 2 - b ** 2 - a ** 2) / (-2.0 * a * b)))
 		
-		angle_a = sss_lofc(side_a, side_b, side_c)
-		angle_b = sss_lofc(side_b, side_c, side_a)
-		angle_c = sss_lofc(side_c, side_a, side_b)
-		assert angle_a + angle_b + angle_c == 180
+		a_a = sss_lofc(s_a, s_b, s_c)
+		a_b = sss_lofc(s_b, s_c, s_a)
+		a_c = sss_lofc(s_c, s_a, s_b)
+		assert a_a + a_b + a_c == 180
 		
-	print(angle_a)
-	print(angle_b)
-	print(angle_c)
+	print(a_a)
+	print(a_b)
+	print(a_c)
 
 
 if __name__ == "__main__":
